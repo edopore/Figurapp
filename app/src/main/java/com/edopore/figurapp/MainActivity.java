5a5e0a9cc18ae0dc6ceb1c0199f545b9ff9da91b
edopore <ediij1994@hotmail.com>
2018-02-24 09:26:03 -0500

    Version final de la figurapp

--- app/src/main/java/com/edopore/figurapp/MainActivity.java
+++ app/src/main/java/com/edopore/figurapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.edopore.figurapp;
 //revisar el por que no funciona el radio button cuando se pone a cambiar los edittext
+
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,13 +13,14 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText eValor,eValor2;
+    EditText eValor, eValor2;
     RadioButton rCuadrado, rCubo, rCirculo, rTriangulo;
     String Arista, Altura;
     Button bCalcular;
     TextView Area, Perimetro, Volumen;
-    TextView Texto1, Texto2;
-    int fop;
+    TextView Texto1, Texto2, tVolumen;
+    int fop = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,36 +36,77 @@ public class MainActivity extends AppCompatActivity {
         Area = findViewById(R.id.eArea);
         Perimetro = findViewById(R.id.ePerimetro);
         Volumen = findViewById(R.id.eVolumen);
-        Texto1 =  findViewById(R.id.tText1);
-        Texto2 =  findViewById(R.id.tText2);
+        Texto1 = findViewById(R.id.tText1);
+        Texto2 = findViewById(R.id.tText2);
+        tVolumen = findViewById(R.id.tVolumen);
 
         Texto1.setText(R.string.arista);
         Texto2.setVisibility(View.INVISIBLE);
+        Volumen.setVisibility(View.INVISIBLE);
+        tVolumen.setVisibility(View.INVISIBLE);
         eValor2.setVisibility(View.INVISIBLE);
+        eValor.setText("");
         eValor2.setText("1");
+        Area.setText("");
+        Perimetro.setText("");
+        Volumen.setText("");
     }
 
     public void onRadioButtonClicked(View view) {
-        int id=view.getId();
-        switch (id){
+        int id = view.getId();
+        switch (id) {
             case R.id.rCuadro:
-                fop=1;
+                fop = 1;
                 Texto1.setText(R.string.arista);
-            break;
+                Texto2.setVisibility(View.INVISIBLE);
+                Volumen.setVisibility(View.INVISIBLE);
+                tVolumen.setVisibility(View.INVISIBLE);
+                eValor2.setVisibility(View.INVISIBLE);
+                eValor2.setText("1");
+                eValor.setText("");
+                Area.setText("");
+                Perimetro.setText("");
+                Volumen.setText("");
+                break;
             case R.id.rCirculo:
-                fop=2;
+                fop = 2;
                 Texto1.setText(R.string.radio);
-            break;
+                Texto2.setVisibility(View.INVISIBLE);
+                Volumen.setVisibility(View.INVISIBLE);
+                tVolumen.setVisibility(View.INVISIBLE);
+                eValor2.setVisibility(View.INVISIBLE);
+                eValor2.setText("1");
+                eValor.setText("");
+                Area.setText("");
+                Perimetro.setText("");
+                Volumen.setText("");
+                break;
             case R.id.rTriangulo:
-                fop=3;
-                Texto2.setVisibility(View.VISIBLE);
+                fop = 3;
                 Texto1.setText(R.string.base);
+                Texto2.setVisibility(View.VISIBLE);
+                Volumen.setVisibility(View.INVISIBLE);
+                tVolumen.setVisibility(View.INVISIBLE);
                 eValor2.setVisibility(View.VISIBLE);
                 eValor2.setText("");
-            break;
+                eValor.setText("");
+                Area.setText("");
+                Perimetro.setText("");
+                Volumen.setText("");
+                break;
             case R.id.rCubo:
-                fop=4;
-            break;
+                fop = 4;
+                Texto1.setText(R.string.arista);
+                Texto2.setVisibility(View.INVISIBLE);
+                Volumen.setVisibility(View.VISIBLE);
+                tVolumen.setVisibility(View.VISIBLE);
+                eValor2.setVisibility(View.INVISIBLE);
+                eValor2.setText("1");
+                eValor.setText("");
+                Area.setText("");
+                Perimetro.setText("");
+                Volumen.setText("");
+                break;
         }
     }
 
@@ -71,39 +114,38 @@ public class MainActivity extends AppCompatActivity {
         Arista = eValor.getText().toString();
         Altura = eValor2.getText().toString();
 
-        if((eValor.getText().toString().isEmpty())||(eValor2.getText().toString().isEmpty())||(eValor.getText().toString().isEmpty() && eValor2.getText().toString().isEmpty())){
-            Toast.makeText(this,"Por Favor ingresa un valor",Toast.LENGTH_SHORT).show();
-        }
-        else if(!eValor.getText().toString().isEmpty()||!eValor2.getText().toString().isEmpty()){
-
-            float num1,num2,area,volumen,perimetro;
-            int a,b;
-            area=volumen=perimetro=0;
-            a=2;
-            b=3;
+        if ((eValor.getText().toString().isEmpty()) || (eValor2.getText().toString().isEmpty()) || (eValor.getText().toString().isEmpty() && eValor2.getText().toString().isEmpty())) {
+            Toast.makeText(this, "Por Favor ingresa un valor", Toast.LENGTH_SHORT).show();
+        } else if (!eValor.getText().toString().isEmpty() || !eValor2.getText().toString().isEmpty()) {
+            float num1, num2, area, volumen, perimetro;
+            int a, b;
+            area = volumen = perimetro = 0;
+            a = 2;
+            b = 3;
             num1 = Float.parseFloat(Arista);
             num2 = Float.parseFloat(Altura);
 
-            switch (fop){
+            switch (fop) {
                 case 1:
-                    area = num1*num1;
-                    perimetro = 4*num1;
+                    area = num1 * num1;
+                    perimetro = 4 * num1;
                     volumen = 0;
                     break;
                 case 2:
-                    area = (float)Math.PI*(float)Math.pow(num1,a);
-                    perimetro = 2*(float)Math.PI*num1;
+                    area = (float) Math.PI * (float) Math.pow(num1, a);
+                    perimetro = 2 * (float) Math.PI * num1;
                     volumen = 0;
                     break;
                 case 3:
-                    area = (num1*num2)/2;
-                    perimetro = 0;
+                    area = (num1 * num2) / 2;
+                    float hip = (float) Math.sqrt((float) Math.pow(num1, a) + (float) Math.pow(num2, a));
+                    perimetro = hip + num1 + num2;
                     volumen = 0;
                     break;
                 case 4:
-                    area = 6*(float)Math.pow(num1,a);
-                    perimetro = 12*(float)Math.pow(num1,a);
-                    volumen = (float)Math.pow(num1,b);
+                    area = 6 * (float) Math.pow(num1, a);
+                    perimetro = 12 * (float) Math.pow(num1, a);
+                    volumen = (float) Math.pow(num1, b);
                     break;
             }
             Area.setText(String.valueOf(area));
